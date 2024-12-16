50165cc9cf47abdb1f0ca0f3a7dd9d2d65d63d95
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2024-12-16 18:47:12 +0200

    Install eslint-plugin-css-modules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,12 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint", "drizzle"],
+  plugins: ["@typescript-eslint", "drizzle", "css-modules"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:css-modules/recommended",
   ],
   rules: {
     "@typescript-eslint/no-unsafe-call": "off",
